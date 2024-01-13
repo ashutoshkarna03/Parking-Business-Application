fb0047641309e71a892f80b1c56a322563e877da
Ashutosh Karna <ashutoshkarna@Ashutoshs-MacBook-Air.local>
2024-01-13 23:10:21 +0100

    chore: code cleanup

--- src/parkingSpace/parkingSpace.controller.ts
+++ src/parkingSpace/parkingSpace.controller.ts
@@ -1,28 +1,20 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
-  Param,
-  Patch,
   Post,
   Res,
   Request
 } from '@nestjs/common';
 import { Response } from 'express';
-import { ParseIntPipe } from '@nestjs/common/pipes/parse-int.pipe';
 import { ParkingSpace, ParkingSession } from './parkingSpace.entity';
 import { ParkingSpaceService } from './parkingSpace.service';
 import { CheckInRequestDto } from './../dtos/checkin.dto';
+import { OccupationResponseDto } from './../dtos/occupation.dto';
 
 @Controller('parkingSpace')
 export class ParkingSpaceController {
   constructor(private parkingSpaceService: ParkingSpaceService) {}
-
-  // @Get()
-  // findAll() {
-  //   return this.parkingSpaceService.getParkingSpace();
-  // }
 
   @Post('/checkIn')
   async checkIn(@Body() body: CheckInRequestDto, @Res() res: Response) {
@@ -55,37 +47,13 @@ export class ParkingSpaceController {
     } catch (err) {
       console.log(err)
     }
-    
-    // res.status(201).json(checkInResponse);
   }
 
   @Get('/occupation')
-  // @HttpCode(HttpStatus.OK)
   async getOccupation(@Request() request): Promise<OccupationResponseDto[]> {
-    // return this.parkingSpaceService.getParkingSpace();
     return await this.parkingSpaceService.paginate({
       page: request.query.hasOwnProperty('page') ? request.query.page : 0,
       limit: request.query.hasOwnProperty('limit') ? request.query.limit : 10,
     });
   }
-
-  // @Get(':id')
-  // findOne(@Param('id', ParseIntPipe) id) {
-  //   return this.parkingSpaceService.findOne(id);
-  // }
-
-  // @Post() create(@Body() parkingSpace: ParkingSpace) {
-  //   return this.parkingSpaceService.createNote(parkingSpace);
-  // }
-
-  // @Patch(':id')
-  // async editNote(@Body() parkingSpace: ParkingSpace, @Param('id') id: number): Promise<ParkingSpace> {
-  //   const noteEdited = await this.parkingSpaceService.editNote(id, parkingSpace);
-  //   return noteEdited;
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id', ParseIntPipe) id) {
-  //   this.parkingSpaceService.remove(id);
-  // }
 }
