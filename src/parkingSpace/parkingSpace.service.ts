b77c9c7624bcbe5a1b1d830d740e9774cbb837c4
Ashutosh Karna <ashutoshkarna@Ashutoshs-MacBook-Air.local>
2024-01-13 22:57:06 +0100

    chore: cleanup

--- src/parkingSpace/parkingSpace.service.ts
+++ src/parkingSpace/parkingSpace.service.ts
@@ -10,6 +10,7 @@ import { uuid } from 'uuidv4';
 export class ParkingSpaceService {
   constructor(
     @InjectRepository(ParkingSpace) private parkingSpaceRepository: Repository<ParkingSpace>,
+    @InjectRepository(ParkingSession) private parkingSessionRepository: Repository<ParkingSession>,
   ) { }
 
   async paginate(
@@ -56,27 +57,4 @@ export class ParkingSpaceService {
       startTime: (Math.floor(+new Date() / 1000)).toString()
     })
   }
-
-  // findOne(id: string): Promise<ParkingSpace> {
-  //   return this.parkingSpaceRepository.findOne(id);
-  // }
-
-  // async createNote(parkingSpace: ParkingSpace) {
-  //   this.parkingSpaceRepository.save(parkingSpace);
-  // }
-
-  // async remove(id: string): Promise<void> {
-  //   await this.parkingSpaceRepository.delete(id);
-  // }
-
-  // async editNote(id: number, parkingSpace: ParkingSpace): Promise<ParkingSpace> {
-  //   const editedNote = await this.parkingSpaceRepository.findOne(id);
-  //   if (!editedNote) {
-  //     throw new NotFoundException('Note is not found');
-  //   }
-  //   editedNote.description = parkingSpace.description;
-  //   editedNote.title = parkingSpace.title;
-  //   await editedNote.save();
-  //   return editedNote;
-  // }
 }
